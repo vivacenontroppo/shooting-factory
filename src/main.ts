a4cd84676d2d9adb1aa008a0e855ea08c722732a
Marian Kucharski <marian.kucharski@itelo.pl>
2019-03-23 00:06:35 +0100

    styles of buttons change to fit mobile view, change to 15 results

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const init = (): void => {
       const word = generator.input.value;
       generator.getRhymeWord(word).then(value => {
         if (value.length > 0) {
-          for (let i = 0; i < 10; i++) {
+          for (let i = 0; i < 15; i++) {
               generator.insertText(`${i + 1}: ${word} rhymes to ${value[i].word}`);
           }
         } else {
@@ -28,7 +28,7 @@ const init = (): void => {
       const word = generator.input.value;
       generator.getSimilarWord(word).then(value => {
         if (value.length > 0) {
-          for (let i = 0; i < 10; i++) {
+          for (let i = 0; i < 15; i++) {
               generator.insertText(`${i + 1}: ${word} is similar to ${value[i].word}`);
           }
         } else {
@@ -47,7 +47,7 @@ const init = (): void => {
       const word = generator.input.value;
       generator.getDescribeWord(word).then(value => {
         if (value.length > 0) {
-          for (let i = 0; i < 10; i++) {
+          for (let i = 0; i < 15; i++) {
               generator.insertText(`${i + 1}: ${word} can be described with ${value[i].word}`);
           }
         } else {
